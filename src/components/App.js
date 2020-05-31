8d46e84d8acc460784b42e902533676d115e2f9c
Konstantin Basmanov <basmanovk@gmail.com>
2020-05-31 15:32:38 -0500

    Origin

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../GlobalStyles.css'
+
+function App() {
+  return (
+    <div className='app container'>
+      <h1>Travel Hangman</h1>
+    </div>
+  )
+}
+
+export default App
