6a24cca97d1b14a10eec5f31e038e7d9a00f0dc9
Konstantin Basmanov <basmanovk@gmail.com>
2020-06-06 11:08:56 -0500

    keyboard complete

--- src/components/NewGame.js
+++ src/components/NewGame.js
@@ -1,30 +1,56 @@
 import React, { Component } from 'react'
 import { allCountries } from '../resources/allCountries'
+import { keyboard } from '../resources/specialCharacters'
 
 export default class NewGame extends Component {
   state = {
     countries: allCountries,
-    currentCountry: null
+    currentCountry: null,
+    capital: null,
+    dash: null,
+    keyboardRows: [
+      { row: keyboard.slice(0, 10) },
+      { row: keyboard.slice(10, 19) },
+      { row: keyboard.slice(19, 26) }
+    ]
   }
 
   componentDidMount() {
-    console.log(this.state.countries)
-    // this.renderCapital('withSpecialCharacters')
+    this.renderCapital()
   }
 
   renderCapital(withSpecChars) {
     withSpecChars = withSpecChars || null
     const randomNumber = Math.floor(Math.random() * this.state.countries.length)
-    const currentCountry = this.state.countries[randomNumber]
+    const pickedCountry = this.state.countries[randomNumber]
+    const currentCountry = pickedCountry.name
     let capital
     withSpecChars
-      ? (capital = currentCountry.capitalSpecial || currentCountry.capital)
-      : (capital = currentCountry.capital)
+      ? (capital = pickedCountry.capitalSpecial || pickedCountry.capital)
+      : (capital = pickedCountry.capital)
 
-    let length = capital.length
-    let dashes = '_ '.repeat(length)
+    const length = capital.length
+    let dash = [...'_'.repeat(length)]
+    capital = [...capital.toLowerCase()]
+    this.setState({ currentCountry, dash, capital }, () => {
+      console.log(this.state.dash)
+    })
+  }
 
-    return dashes
+  handleChoose = key => {
+    console.log('clicked:', key)
+    const capital = [...this.state.capital]
+    const dash = this.state.dash
+    capital.forEach(letter => {
+      if (letter === key) {
+        // console.log(dash)
+        const index = capital.indexOf(letter)
+        dash[index] = key
+        this.setState({ capital, dash })
+      } else {
+        return console.log('Wrong letter!')
+      }
+    })
   }
 
   render() {
@@ -32,7 +58,25 @@ export default class NewGame extends Component {
       <div>
         <h1>New Game</h1>
         <div className='container'>
-          <h3>{this.renderCapital()}</h3>
+          <h3>What is the capital of {this.state.currentCountry}?</h3>
+          {this.state.dash.map(item => (
+            <h3 className='capital' key={this.renderDash().indexOf(item)}>
+              {item}
+            </h3>
+          ))}
+          <div className='container'>
+            {this.state.keyboardRows.map(obj => (
+              <ul className='keyboard flex' key={obj.row.toString()}>
+                {obj.row.map(letter => (
+                  <button key={letter}>
+                    <li onClick={() => this.handleChoose(letter)}>
+                      {letter.toUpperCase()}
+                    </li>
+                  </button>
+                ))}
+              </ul>
+            ))}
+          </div>
         </div>
       </div>
     )
---
+++ src/resources/specialCharacters.js
@@ -0,0 +1,103 @@
+export const keyboard = [...'qwertyuiopasdfghjklzxcvbnm']
+
+export const allSpecialChar = [
+  "'",
+  '-',
+  'í',
+  'é',
+  'á',
+  'ó',
+  'ē',
+  'ç',
+  '.',
+  'å',
+  'ș',
+  'ã',
+  ',',
+  'ú'
+]
+
+export const ponctuation = ["'", '-', '.', ',']
+
+export const substitutionArray = [
+  'í',
+  'é',
+  'á',
+  'ó',
+  'ē',
+  'ç',
+  'å',
+  'ș',
+  'ã',
+  'ú'
+]
+
+export const substitution = [
+  { old: 'í', new: 'i' },
+  { old: 'é', new: 'e' },
+  { old: 'á', new: 'a' },
+  { old: 'ó', new: 'o' },
+  { old: 'ē', new: 'e' },
+  { old: 'ç', new: 'c' },
+  { old: 'å', new: 'a' },
+  { old: 'ș', new: 's' },
+  { old: 'ã', new: 'a' },
+  { old: 'ú', new: 'u' }
+]
+
+export const specialCharCapitals = [
+  'Brasília',
+  'Yaoundé',
+  'Bogotá',
+  'San José',
+  'Tórshavn',
+  'Papeetē',
+  'Port-aux-Français',
+  'Hagåtña',
+  'Reykjavík',
+  'Malé',
+  'Nouméa',
+  'Asunción',
+  'Chișinău',
+  'São Tomé',
+  'Lomé',
+  'El Aaiún'
+]
+
+export const normalCharCapitals = [
+  'Brasilia',
+  'Yaounde',
+  'Bogota',
+  'San Jose',
+  'Torshavn',
+  'Papeete',
+  'Port-aux-Francais',
+  'Hagatna',
+  'Reykjavik',
+  'Male',
+  'Noumea',
+  'Asuncion',
+  'Chisinau',
+  'Sao Tome',
+  'Lome',
+  'El Aaiun'
+]
+
+export const capitals = [
+  { standard: 'Brasilia', special: 'Brasília' },
+  { standard: 'Yaounde', special: 'Yaoundé' },
+  { standard: 'Bogota', special: 'Bogotá' },
+  { standard: 'San Jose', special: 'San José' },
+  { standard: 'Torshavn', special: 'Tórshavn' },
+  { standard: 'Papeete', special: 'Papeetē' },
+  { standard: 'Port-aux-Francais', special: 'Port-aux-Français' },
+  { standard: 'Hagatna', special: 'Hagåtña' },
+  { standard: 'Reykjavik', special: 'Reykjavík' },
+  { standard: 'Male', special: 'Malé' },
+  { standard: 'Noumea', special: 'Nouméa' },
+  { standard: 'Asuncion', special: 'Asunción' },
+  { standard: 'Chisinau', special: 'Chișinău' },
+  { standard: 'Sao Tome', special: 'São Tomé' },
+  { standard: 'Lome', special: 'Lomé' },
+  { standard: 'El Aaiun', special: 'El Aaiún' }
+]
